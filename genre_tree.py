c991e99cd7591e381799623dab53f7c4be88cbba
Tianqi Pan <pantianqi666@gmail.com>
2026-03-22 17:47:00 -0400

    added genre_tree and song_graph

--- genre_tree.py
+++ genre_tree.py
@@ -1,2 +1,11 @@
-class _Genre:
+from __future__ import annotations
+import csv
+from typing import Any
+
+class Genre_Tree:
+
+    genre: str
+    songs: list[str]
+    _subtrees: list[Genre_Tree]
+
 

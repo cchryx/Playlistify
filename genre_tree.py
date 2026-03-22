66f8d85076a93f1531b2b648554fccf08b154e3e
Tianqi Pan <pantianqi666@gmail.com>
2026-03-22 18:13:39 -0400

    update genre_tree.py

--- genre_tree.py
+++ genre_tree.py
@@ -1,11 +1,24 @@
 from __future__ import annotations
 import csv
-from typing import Any
+from typing import Any, Optional
 
 class Genre_Tree:
 
-    genre: str
+    _genre: str
     songs: list[str]
     _subtrees: list[Genre_Tree]
+
+    def __init__(self, genre: Optional[Any], subtrees: list[Genre_Tree]):
+        self._genre = genre
+        self._subtrees = subtrees
+
+    def is_empty(self) -> bool:
+        return self._genre is None
+
+    def add_subtree(self, subtree: Genre_Tree) -> None:
+        self._subtrees.append(subtree)
+
+    def get_subtrees(self) -> list[Genre_Tree]:
+        return self._subtrees
 
 

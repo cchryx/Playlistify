6abe458a3cc7b9046c06c72b9ff7638e60715f03
enMaesineD <denisemhma2006@gmail.com>
2026-03-24 21:47:54 -0400

    hi

---
+++ data/user_input.py
@@ -0,0 +1,12 @@
+import genre_tree
+
+genre_tree.create_genre_tree()
+
+genre = input("Enter the genre you want (enter the exact genre or sub-genre)")
+
+
+
+bool_viral_song = input("Do you like viral songs?")
+
+energy_level = input("Enter your energy level from 1 - 10")
+

d2147f7e076588d184733c77afe75b85781c115f
Tianqi Pan <pantianqi666@gmail.com>
2026-04-01 14:31:03 -0400

    Revert "Merge pull request #7 from cchryx/Chris"
    This reverts commit b2b6fd61926e33b1aaf89bdcb3e0dcd35c999259, reversing
    changes made to fe9208babb9db556ce530828a99e0ac914c18900.

--- data/data_extraction/downloaded_data.py
+++
@@ -1,8 +0,0 @@
-# Used to download the song dataset file from Kaggle
-import kagglehub
-import os
-
-os.environ['KAGGLE_CONFIG_DIR'] = os.path.expanduser("~/.kaggle")
-
-path = kagglehub.dataset_download("amitanshjoshi/spotify-1million-tracks")
-print("Path to dataset files:", path)
--- main.py
+++
@@ -1,9 +0,0 @@
-"""
-CSC111 Project: Playlistify (Mood-Aware Music Recommendation Engine)
-
-This is the main entry point for Playlistify. It loads song and genre data,
-filters songs based on simulated user preferences, and builds a candidate
-playlist for further recommendation.
-
-Copyright (c) 2026 Xing Xu Chen, Tianqi Pan, Norah Liu, Denise Ma
-"""
